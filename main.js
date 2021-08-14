c7553b05765eac892d55a7ed4251182e3c313aab
AarushSarda <77834111+AarushSarda@users.noreply.github.com>
2021-08-14 15:35:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+function setup() {
+  canvas = createCanvas(200, 200);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+  classifier = ml5.imageClassifier('MobileNet' , modelLoaded);
+}
+function modelLoaded() {
+  console.log ("Model is Loaded");
+}
+function draw() {
+  image(video,0,0,200,200);
+  classifier.classify(video,gotResult);
+}
+function gotResult(error , results) {
+  if (error){
+    console.error(error);
+  }
+  else {
+    console.log(results);
+    document.getElementById("result_object_name").innerHTML = results[0].label;
+    var accuracy = results[0].confidence*100 ; 
+    document.getElementById("result_object_accuracy").innerHTML = accuracy.toFixed(0) + "%" ;
+  }
+}
+
+
+
